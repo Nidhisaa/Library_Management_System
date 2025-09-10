c217a872f1023ce1a1df863e384435accd97c381
Nidhisaa <senthilnidhi57@gmail.com>
2025-09-10 06:17:14 +0530

    dotenv done

--- datebase/db.js
+++ datebase/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 async function connectdb() {
-  const connection = mongoose.connect(
-    "mongodb+srv://test_user_a:yVjZ7ONGFB173YmP@cluster0.a82xa8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  const connection = mongoose.connect(process.env.MONGODB_URL);
   console.log("Database connected");
 }
 module.exports = connectdb;
