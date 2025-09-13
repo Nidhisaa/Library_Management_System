0e19c243a352e67a674f112e7d2df19d872209a6
Nidhisaa <senthilnidhi57@gmail.com>
2025-09-13 09:22:53 +0530

    Time updated

--- models/schema.js
+++ models/schema.js
@@ -4,6 +4,7 @@ const Lib = new Schema({
   id: { type: Number, index: true },
   name: { type: String, index: true },
   author: { type: String },
+   time: { type: Date, default: () => new Date(Date.now() + 5.5 * 60 * 60 * 1000) },
   avalibility:{type: Boolean,default: true}
 
 });
